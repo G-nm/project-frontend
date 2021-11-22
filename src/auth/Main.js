d9df00cddf25a50ff1d612db9e87c16e856464bb
G-nm <gmang2236@gmail.com>
2021-11-22 23:50:30 +0300

    fixes

--- src/auth/Main.js
+++ src/auth/Main.js
@@ -5,59 +5,60 @@ import Login from "./Login";
 import { ShowContext } from "./Showcontext";
 
 const Main = (props) => {
-  const [show, setShow] = React.useState(true);
-  const [erroralert, setErrorAlert] = React.useState("");
-  useEffect(() => {
-    setTimeout(() => {
-      setErrorAlert("");
-    }, 5000);
-  }, [erroralert]);
+	const [show, setShow] = React.useState(true);
+	const [erroralert, setErrorAlert] = React.useState("");
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setErrorAlert("");
+		}, 5000);
+		return () => clearTimeout(timeout);
+	}, [erroralert]);
 
-  return (
-    <>
-      <div className="flex justify-center ">
-        <div className="bg-red-500 w-2/5 text-center text-white">
-          {erroralert}
-        </div>
-      </div>
-      <div className="flex justify-center  h-full font-sans  my-8">
-        <div className="shadow-2xl md:w-3/12  h-full ">
-          <div className="">
-            <button
-              className={
-                !show
-                  ? " w-1/2 text-center bg-gray-900 text-white float-left h-16 text-2xl outline-none focus:outline-none shadow-lg"
-                  : " w-1/2 text-center bg-white float-left h-16 text-2xl outline-none focus:outline-none "
-              }
-              onClick={(e) => {
-                setShow(false);
-              }}
-            >
-              <span className="font-semibold">LOG IN</span>
-            </button>
-            <button
-              className={
-                show
-                  ? " w-1/2 text-center bg-gray-900 text-white float-right h-16 text-2xl outline-none focus:outline-none shadow-lg"
-                  : " w-1/2 text-center bg-white float-right h-16 text-2xl outline-none focus:outline-none "
-              }
-              onClick={() => {
-                setShow(true);
-              }}
-            >
-              <span className="font-semibold">SIGN UP</span>
-            </button>
-          </div>
-          <div className="w-full px-12 pb-10  h-full bg-white">
-            <ShowContext.Provider value={{ setShow, setErrorAlert }}>
-              {show && <Signup />}
-              {!show && <Login routerprops={props} />}
-            </ShowContext.Provider>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex justify-center ">
+				<div className="bg-red-500 w-2/5 text-center text-white">
+					{erroralert}
+				</div>
+			</div>
+			<div className="flex justify-center  h-full font-sans  my-8">
+				<div className="shadow-2xl md:w-3/12  h-full ">
+					<div className="">
+						<button
+							className={
+								!show
+									? " w-1/2 text-center bg-gray-900 text-white float-left h-16 text-2xl outline-none focus:outline-none shadow-lg"
+									: " w-1/2 text-center bg-white float-left h-16 text-2xl outline-none focus:outline-none "
+							}
+							onClick={(e) => {
+								setShow(false);
+							}}
+						>
+							<span className="font-semibold">LOG IN</span>
+						</button>
+						<button
+							className={
+								show
+									? " w-1/2 text-center bg-gray-900 text-white float-right h-16 text-2xl outline-none focus:outline-none shadow-lg"
+									: " w-1/2 text-center bg-white float-right h-16 text-2xl outline-none focus:outline-none "
+							}
+							onClick={() => {
+								setShow(true);
+							}}
+						>
+							<span className="font-semibold">SIGN UP</span>
+						</button>
+					</div>
+					<div className="w-full px-12 pb-10  h-full bg-white">
+						<ShowContext.Provider value={{ setShow, setErrorAlert }}>
+							{show && <Signup />}
+							{!show && <Login routerprops={props} />}
+						</ShowContext.Provider>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Main;
